909a45998579c9f226cfd914e929d4b3585415d6
Luis Rocha <senseyluiz@gmail.com>
2020-11-05 19:03:47 -0300

    Aulas JavaScript

---
+++ JavaScript-introducao/aulas/js/main.js
@@ -0,0 +1,52 @@
+// var nome = "Luis Henrique";
+// var idade = 37;
+// var frase = "Japão é o melhor time."
+// console.log(nome + " tem " + idade + " anos");
+
+// console.log(frase.replace("Japão", "Brasil"));
+// console.log(frase.toUpperCase("Japão", "Brasil"));
+// console.log(frase.toLowerCase("Japão", "Brasil"));
+
+// 
+
+// var idade = prompt("Qual sua idade: ")
+// if(idade > 18){
+// 	alert("Maior de idade");
+// }
+// else{
+// 	alert("Menor de idade");
+// }
+
+
+// var count = 0;
+// while (count < 5){
+// 	console.log(count);
+// 	count ++;
+// }
+
+// for(count=0; count <= 5; count++){
+// 	console.log(count)
+// }
+
+// 
+
+
+// function soma(n1, n2){
+// 	return (n1 + n2);
+// }
+
+// console.log(soma(8, 3));
+
+// function clicou(){
+// 	document.getElementById('agradecimento').innerHTML = "Obrigado por Clicar";
+// }
+
+// function redirecionando(){
+// 	window.open("https://github.com/senseyluiz/html-web-developer");
+// }
+
+let line = gets(117).split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = 0; // Altere o valor da variável com o cálculo esperado
+console.log("X = " + total);
